254416e0c9ba8a4adfb15f2c43d4556e6fce9976
Alexander Quatrini <alexanderquatrini@gmail.com>
2024-01-08 07:59:45 -0600

    Adds nonfunctional nav arrows to stats

--- src/components/RollStats/RollStats.js
+++ src/components/RollStats/RollStats.js
@@ -10,13 +10,19 @@ class RollStats extends React.Component{
         <div className='roll-stats'>
             <h1>Stats</h1>
             
-            <div className='last-five'>
+            <div className='roll-history-container'>
 
-                <h2>Last 5 Rolls</h2>
-                <ul>{RollList.slice(-5).map(item => {
-                    return <li className='last-five-results' key={item.date}><p className='values'> {item.value} </p> <br /> <p>{item.date}</p> </li>
-                })}
-                </ul>
+                <h2>Roll History</h2>
+                <div className='roll-history'>
+                    <span className='nav'>&#10094;</span>
+                    
+                    <ul>{RollList.slice(-5).map(item => {
+                        return <li className='last-five-results' key={item.date}><p className='values'> {item.value} </p> <br /> <p>{item.date}</p> </li>
+                    })}
+                    </ul>
+                    
+                    <span className='nav'>&#10095;</span>
+                </div>
                 
             </div>
         </div>
