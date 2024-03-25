96c5fa524b9c64922bf13619626d50983296fbd2
Alexander Quatrini <alexanderquatrini@gmail.com>
2024-03-25 10:13:58 -0500

    Finish converting to functional components

--- src/components/GeneralStats/general-stats.js
+++ src/components/GeneralStats/general-stats.js
@@ -3,45 +3,37 @@ import './general-stats.css'
 import RollList from '../roll-list';
 import RollDisplay from '../RollDisplay/RollDisplay';
 
-class GeneralStats extends React.Component{
+function GeneralStats(props){
     
-    constructor(props){
-        super(props);
-        this.rollEntry = props.rollEntryDate;
-        this.error = false;
-        this.calculateStats();
-    }
+    let entryStats = calculateStats(props.rollEntryDate);
 
-    calculateStats(){
-        let index = RollList.findIndex(item => this.rollEntry == item.date);
-        if(index == -1){
-            this.error = true;
-        } else{
-            this.error = false;
-            this.entry = RollList[index];
-            let before = RollList.slice(0, index);
-            let after = RollList.slice(index);
-            console.log(before);
-            console.log(after);
-        }
-    }
-
-    render(){
-        if(this.error){
-            return(
-                <div className='general-error-container'>
-                    <h3>Something went wrong...</h3>
-                    <p>Looks like we can't find this entry! '{this.entry.date}'</p>
-                </div>
-            );
-        }
-        else return(
-            <div className='general-stat-container'>
-                <h3>{this.entry.date}</h3>
-                <RollDisplay value= {this.entry.value}></RollDisplay>
-                
+    if(entryStats === undefined){
+        return(
+            <div className='general-error-container'>
+                <h3>Something went wrong...</h3>
+                <p>Looks like we can't find this entry! '{props.rollEntryDate}'</p>
             </div>
         );
+    }
+    else return(
+        <div className='general-stat-container'>
+            <h3>{entryStats.entry.date}</h3>
+            <RollDisplay value= {entryStats.entry.value}></RollDisplay>
+            
+        </div>
+    );
+}
+
+function calculateStats(rollDate){
+    let index = RollList.findIndex(item => rollDate == item.date);
+    if(index == -1){
+        return undefined
+    }else{
+        return({
+            entry: RollList[index],
+            before: RollList.slice(0, index),
+            after: RollList.slice(index),
+        })
     }
 }
 
